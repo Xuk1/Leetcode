a42d0543074ef43e76512e410476fd08053d2ccf
Xuk1 <58908251+Xuk1@users.noreply.github.com>
2021-08-22 13:39:34 +0800

    Create 789. 逃脱阻碍者.java
    执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
    内存消耗：37.9 MB, 在所有 Java 提交中击败了73.53%的用户
    
    看了官方题解，使用曼哈顿距离来判断：
      曼哈顿距离：Distance = | x_target - x_0 | + | y_target - y_0 |
    1. 当逃脱者到目的地的曼哈顿距离 > ghosts到目的地的曼哈顿距离：
    		ghosts会比逃脱者先到目的地
    2. 当逃脱者到目的地的曼哈顿距离 = ghosts到目的地的曼哈顿距离：
    		ghosts会跟逃脱者同时到目的地
    3. 逃脱者到目的地的曼哈顿距离 < ghosts到目的地的曼哈顿距离：
    		逃脱者能够逃脱成功

---
+++ 789. 逃脱阻碍者.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int myDis = Math.abs(target[0]) + Math.abs(target[1]);      //计算逃脱者到目的地的曼哈顿距离
+        int[] gDis = new int[ghosts.length];
+        boolean flag = true;
+        for(int i=0; i<ghosts.length; i++){                         //计算每个ghost到目的地的曼哈顿距离
+            gDis[i] = Math.abs(ghosts[i][0]-target[0]) + Math.abs(ghosts[i][1]-target[1]);
+            if(gDis[i] <= myDis){                                   //如果ghosts的曼哈顿距离小于等于逃脱者的曼哈顿距离，则逃脱不成功
+                flag = false;
+            }
+        }
+        return flag;
+    }
+}
