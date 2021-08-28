c8bbc4e45dd2b9890fd0c73035ad645ae810b69d
Xuk1 <58908251+Xuk1@users.noreply.github.com>
2021-08-28 20:18:04 +0800

    Update 1480. 一维数组的动态和.java

--- 1480. 一维数组的动态和.java
+++ 1480. 一维数组的动态和.java
@@ -1,6 +1,18 @@
-
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        int[] sum = new int[n];
+        Arrays.fill(sum, 0);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                sum[i] += nums[j];
+            }
+        }
+        return sum;
+    }
+}
 
 /*
-执行用时：6 ms, 在所有 Java 提交中击败了7.14%的用户
-内存消耗：38.8 MB, 在所有 Java 提交中击败了9.60%的用户
+执行用时:5 ms ,在所有Java提交中击败了7.14%的用户
+内存消耗:38.6 MB ，在所有Java提交中击败了40.22%的用户
 */
